ef3152f4820241891a17481a6dcade159c3aec70
github-action-benchmark <github@users.noreply.github.com>
2025-11-06 19:31:20 +0000

    add Benchmark (ndjson) benchmark result for 01202304f022bf0ca6bb188c857ea6677e6aa20e

--- perf/results/j_dateformatperf/en_US/TestICUParse/1/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/1/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762453730422,
+  "lastUpdate": 1762457480891,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -41007,6 +41007,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 330.26891212318566,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "01202304f022bf0ca6bb188c857ea6677e6aa20e",
+          "message": "ICU-23248 unset sensitive on tickets after one release not two",
+          "timestamp": "2025-11-06T11:19:38-08:00",
+          "tree_id": "b4b7150aee2aded96529d9c5f9c6b44611221892",
+          "url": "https://github.com/unicode-org/icu/commit/01202304f022bf0ca6bb188c857ea6677e6aa20e"
+        },
+        "date": 1762457110641,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 341.96462409180845,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
