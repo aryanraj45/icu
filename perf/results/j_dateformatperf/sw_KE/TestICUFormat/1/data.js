aae7e820dc4039fedec90802c71063b818c171ce
github-action-benchmark <github@users.noreply.github.com>
2025-11-06 18:28:02 +0000

    add Benchmark (ndjson) benchmark result for 321a7bcd872e16e2cd82b50655278c87cdd009e5

--- perf/results/j_dateformatperf/sw_KE/TestICUFormat/1/data.js
+++ perf/results/j_dateformatperf/sw_KE/TestICUFormat/1/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762388324872,
+  "lastUpdate": 1762453682532,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -40827,6 +40827,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 265.03045835636686,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "distinct": true,
+          "id": "321a7bcd872e16e2cd82b50655278c87cdd009e5",
+          "message": "ICU-23247 Bump the github-actions group with 3 updates\n\nSee #3760",
+          "timestamp": "2025-11-06T10:08:52-08:00",
+          "tree_id": "63189f76b16e34458ba29dd65b4cc6c7d2e5082c",
+          "url": "https://github.com/unicode-org/icu/commit/321a7bcd872e16e2cd82b50655278c87cdd009e5"
+        },
+        "date": 1762453319075,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 263.4651187296631,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
